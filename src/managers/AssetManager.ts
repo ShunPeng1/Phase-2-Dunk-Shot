7c8d2b61188a2a86716752b456f90c5e5de8d865
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-07-01 09:07:54 +0700

    feat: Managers

--- src/managers/AssetManager.ts
+++ src/managers/AssetManager.ts
@@ -8,7 +8,8 @@ class AssetManager {
     public static readonly TUTORIALS_FOLDER: string = AssetManager.DUNK_SHOT_IMAGE_FOLDER + "tutorials/";
     public static readonly UI_FOLDER: string = AssetManager.DUNK_SHOT_IMAGE_FOLDER + "ui/";
     public static readonly WHEEL_FOLDER: string = AssetManager.DUNK_SHOT_IMAGE_FOLDER + "wheel/";
-    
+    public static readonly TITLES_FOLDER: string = AssetManager.DUNK_SHOT_IMAGE_FOLDER + "titles/";
+
     // Balls images
     public static readonly BASKETBALL_IMAGE: string = AssetManager.BALLS_FOLDER + "17.png"; 
     public static readonly BASKETBALL_KEY: string = "basketball";
@@ -27,6 +28,17 @@ class AssetManager {
     public static readonly TRAJECTORY_IMAGE: string = AssetManager.MASKS_FOLDER + "45.png";
     public static readonly TRAJECTORY_KEY: string = "trajectory";
 
+
+    // Title images
+    public static readonly MAIN_MENU_TITLE_IMAGE: string = AssetManager.TITLES_FOLDER + "10.png";
+    public static readonly MAIN_MENU_TITLE_KEY: string = "main menu title";
+
+    public static readonly MOBILE_TITLE_IMAGE: string = AssetManager.TITLES_FOLDER + "172.png";
+    public static readonly MOBILE_TITLE_KEY: string = "mobile title";
+
+
+    
+
     // Sounds
     public static readonly DUNK_SHOT_SOUND_FOLDER: string = "assets/sounds/dunk-shot/";
 
@@ -34,7 +46,7 @@ class AssetManager {
     // Scenes
     public static readonly LOADING_SCENE: string = "LoadingScene";
     public static readonly PLAY_SCENE: string = "PlayScene";
-    
+    public static readonly MAIN_MENU_UI_SCENE: string = "MainMenuUIScene";
 
     
 
---
+++ src/managers/GameStateManager.ts
@@ -0,0 +1,24 @@
+import { Scene } from "phaser";
+import AssetManager from "./AssetManager";
+
+class GameStateManager extends Phaser.Events.EventEmitter {
+    private scene : Scene;
+    
+    
+    constructor(scene : Scene) {
+        super();
+        
+        this.scene = scene;
+    }
+
+
+    public loadMainMenuUI() : void {
+        this.scene.scene.launch(AssetManager.MAIN_MENU_UI_SCENE);
+        
+    }
+
+
+}
+
+
+export default GameStateManager;
