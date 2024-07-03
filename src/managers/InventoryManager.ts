5544929b618c99f061f4e2c0404cedf775b7bd65
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-07-03 15:09:10 +0700

    feat: subcriber for Inventory Manager

--- src/managers/InventoryManager.ts
+++ src/managers/InventoryManager.ts
@@ -1,9 +1,11 @@
 class InventoryManager {
     private static instance: InventoryManager;
     private inventory: { [key: string]: number };
+    private subscribers: { [key: string]: Array<(from : number, to: number) => void> };
 
     private constructor() {
         this.inventory = {};
+        this.subscribers = {}; 
         this.loadInventory();
     }
 
@@ -16,19 +18,29 @@ class InventoryManager {
 
     public addItem(item: string, quantity: number) {
         if (this.inventory[item]) {
+            let from = this.inventory[item];
             this.inventory[item] += quantity;
+            this.notifySubscribers(item, from);
         } else {
+            let from = 0;
             this.inventory[item] = quantity;
+            this.notifySubscribers(item, from);
         }
+        
         this.saveInventory();
     }
 
     public removeItem(item: string, quantity: number) {
         if (this.inventory[item] && this.inventory[item] >= quantity) {
+            let from = this.inventory[item];
             this.inventory[item] -= quantity;
+
+            
+            this.notifySubscribers(item, from);
             if (this.inventory[item] === 0) {
                 delete this.inventory[item];
             }
+            
             this.saveInventory();
         }
     }
@@ -40,6 +52,31 @@ class InventoryManager {
     public getInventory() {
         return this.inventory;
     }
+
+    
+    public subscribe(item: string, callback: (quantity: number) => void) {
+        if (!this.subscribers[item]) {
+            this.subscribers[item] = [];
+        }
+        this.subscribers[item].push(callback);
+    }
+
+    public unsubscribe(item: string, callback: (quantity: number) => void) {
+        if (this.subscribers[item]) {
+            const index = this.subscribers[item].indexOf(callback);
+            if (index !== -1) {
+                this.subscribers[item].splice(index, 1);
+            }
+        }
+    }
+
+    private notifySubscribers(item: string, from : number) {
+        if (this.subscribers[item]) {
+            const to = this.inventory[item] || 0;
+            this.subscribers[item].forEach(callback => callback(from, to));
+        }
+    }
+
 
     private saveInventory() {
         localStorage.setItem('inventory', JSON.stringify(this.inventory));
